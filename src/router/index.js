65d5af5ec0825793708c6a763cce82973f6cb2e1
Murilo Angelo <muriloangelo10@gmail.com>
2021-03-26 16:42:32 -0300

    adding routes and their pages and middleware

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
+import store from "./../store";
 
 Vue.use(VueRouter);
 
@@ -11,13 +12,19 @@ const routes = [
     component: Home,
   },
   {
-    path: "/about",
-    name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue"),
+    path: "/login",
+    name: "Login",
+    component: () => import("../views/Login.vue"),
+  },
+  {
+    path: "/register",
+    name: "Register",
+    component: () => import("../views/Register.vue"),
+  },
+  {
+    path: "/home",
+    name: "Home",
+    component: () => import("../views/Home.vue"),
   },
 ];
 
@@ -25,6 +32,12 @@ const router = new VueRouter({
   mode: "history",
   base: process.env.BASE_URL,
   routes,
+});
+
+router.beforeEach((to, from, next) => {
+  if (to.name !== "Login" && !store.getters["auth/isAuthenticated"])
+    next({ name: "Login" });
+  else next();
 });
 
 export default router;
