56b1b442f9993e2e507c78a933cf6958e3854ad8
Murilo Angelo <muriloangelo10@gmail.com>
2021-03-30 20:14:45 -0300

    adding navbar component

--- src/App.vue
+++ src/App.vue
@@ -1,51 +1,19 @@
 <template>
   <div id="app">
+    <nav-bar></nav-bar>
     <div id="nav">
       <router-link to="/">Mesa</router-link> |
-      <router-link to="/login">Login</router-link> |
-      <router-link to="/register">Register</router-link> |
       <router-link to="/home">Home</router-link> |
       <router-link to="/profile">Profile</router-link> |
-      <button @click.prevent="callLogout()" v-if="$store.state.auth.token">
-        Logout
-      </button>
     </div>
     <router-view />
   </div>
 </template>
 <script>
-import { mapActions } from "vuex";
+import NavBar from "./components/NavBar.vue";
 
 export default {
   name: "App",
-  methods: {
-    ...mapActions("auth", ["logout"]),
-    callLogout() {
-      this.logout().then(() => this.$router.push({ name: "Login" }));
-    },
-  },
+  components: { NavBar },
 };
 </script>
-
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
-</style>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <b-navbar toggleable="lg" type="dark" variant="primary">
+      <b-navbar-brand to="/">NavBar</b-navbar-brand>
+
+      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+      <b-collapse id="nav-collapse" is-nav>
+        <b-navbar-nav class="ml-auto">
+          <template v-if="!$store.state.auth.token">
+            <b-nav-item to="/login">Entrar</b-nav-item>
+            <b-nav-item to="/register">Cadastrar</b-nav-item>
+          </template>
+          <b-nav-item-dropdown right v-else>
+            <template #button-content>
+              <em>User</em>
+            </template>
+            <b-dropdown-item to="/profile">Perfil</b-dropdown-item>
+            <b-dropdown-item to="/favorites">Favoritos</b-dropdown-item>
+            <b-dropdown-item @click.prevent="callLogout()"
+              >Sair</b-dropdown-item
+            >
+          </b-nav-item-dropdown>
+        </b-navbar-nav>
+      </b-collapse>
+    </b-navbar>
+  </div>
+</template>
+
+<script>
+import { mapActions } from "vuex";
+export default {
+  name: "NavBar",
+  props: {},
+  methods: {
+    ...mapActions("auth", ["logout"]),
+    callLogout() {
+      this.logout().then(() => this.$router.push({ name: "Login" }));
+    },
+  },
+};
+</script>
+
+<style lang="scss"></style>
