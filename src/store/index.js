99a8d49a145dc99b4eb69fb612543503d2256e5b
Murilo Angelo <muriloangelo10@gmail.com>
2021-03-30 16:17:10 -0300

    adding place module to vuex

--- src/store/index.js
+++ src/store/index.js
@@ -2,12 +2,14 @@ import Vue from "vue";
 import Vuex from "vuex";
 import * as auth from "./auth";
 import * as user from "./user";
+import * as place from "./place";
 
 Vue.use(Vuex);
 
 const modules = {
   auth: { ...auth, namespaced: true },
   user: { ...user, namespaced: true },
+  place: { ...place, namespaced: true },
 };
 
 export default new Vuex.Store({
