9779e0d55ec966438f988e7bd341f0f2a746deeb
Murilo Angelo <muriloangelo10@gmail.com>
2021-04-04 15:56:00 -0300

    adding validations to profile form and disabling submit button

--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -4,39 +4,57 @@
       <aside class="col-sm-4">
         <div class="card">
           <article class="card-body">
-            <h4 class="card-title mb-4 mt-1">Perfil</h4>
+            <h4 class="card-title mb-4 mt-1 text-center">Perfil</h4>
             <b-alert variant="danger" :show="hasError" dismissible>
               {{ profileFormError }}
             </b-alert>
             <form id="profileForm" @submit.prevent="sendUpdateUser">
-              <div class="form-group input-group">
+              <div class="form-group input-group justify-content-center">
+                <b-avatar
+                  :src="$store.state.auth.loggedUser.avatar"
+                  size="6rem"
+                ></b-avatar>
+              </div>
+              <div class="form-group">
                 <input
                   name="first_name"
                   class="form-control"
+                  :class="invalidInput('first_name')"
                   placeholder="Nome"
                   type="text"
-                  v-model="profileForm.first_name"
+                  v-model="$v.profileForm.first_name.$model"
                 />
+                <div class="text-danger" v-if="$v.profileForm.last_name.$error">
+                  <span>Campo obrigatório</span>
+                </div>
               </div>
-              <div class="form-group input-group">
+              <div class="form-group">
                 <input
                   name="last_name"
                   class="form-control"
+                  :class="invalidInput('last_name')"
                   placeholder="Sobrenome"
                   type="text"
-                  v-model="profileForm.last_name"
+                  v-model="$v.profileForm.last_name.$model"
                 />
+                <div class="text-danger" v-if="$v.profileForm.last_name.$error">
+                  <span>Campo obrigatório</span>
+                </div>
               </div>
-              <div class="form-group input-group">
+              <div class="form-group">
                 <input
                   name="email"
                   class="form-control"
+                  :class="invalidInput('email')"
                   placeholder="Email address"
                   type="email"
-                  v-model="profileForm.email"
+                  v-model="$v.profileForm.email.$model"
                 />
+                <div class="text-danger" v-if="$v.profileForm.last_name.$error">
+                  <span>Campo obrigatório</span>
+                </div>
               </div>
-              <div class="form-group input-group">
+              <div class="form-group">
                 <input
                   name="phone"
                   class="form-control"
@@ -45,7 +63,7 @@
                   v-model="profileForm.phone"
                 />
               </div>
-              <div class="form-group input-group">
+              <div class="form-group">
                 <select class="form-control" v-model="profileForm.role">
                   <option selected="">Selecione sua profissão</option>
                   <option>Designer</option>
@@ -54,7 +72,11 @@
                 </select>
               </div>
               <div class="form-group">
-                <button type="submit" class="btn btn-primary btn-block">
+                <button
+                  type="submit"
+                  class="btn btn-primary btn-block"
+                  :disabled="$v.profileForm.$invalid"
+                >
                   Atualizar informações
                 </button>
               </div>
@@ -68,6 +90,7 @@
 
 <script>
 import { mapActions } from "vuex";
+import { required, email } from "vuelidate/lib/validators";
 
 export default {
   name: "Profile",
@@ -84,18 +107,36 @@ export default {
       },
     };
   },
+  validations: {
+    profileForm: {
+      first_name: { required },
+      last_name: { required },
+      email: {
+        required,
+        email,
+      },
+    },
+  },
   mounted() {
     this.fillForm();
   },
   methods: {
     ...mapActions("user", ["updateUserData"]),
     sendUpdateUser() {
-      this.updateUserData(this.profileForm)
-        .then(() => this.fillForm())
-        .catch((error) => (this.profileFormError = error.response.data.error));
+      this.$v.$touch();
+
+      if (!this.$v.profileForm.$invalid)
+        this.updateUserData(this.profileForm)
+          .then(() => this.fillForm())
+          .catch(
+            (error) => (this.profileFormError = error.response.data.error)
+          );
     },
     fillForm() {
       this.profileForm = Object.assign({}, this.$store.state.auth.loggedUser);
+    },
+    invalidInput(field) {
+      return this.$v.profileForm[field].$error ? "is-invalid" : "";
     },
   },
   computed: {
--- src/views/Register.vue
+++ src/views/Register.vue
@@ -100,7 +100,11 @@
                 </div>
               </div>
               <div class="form-group">
-                <button type="submit" class="btn btn-primary btn-block">
+                <button
+                  type="submit"
+                  class="btn btn-primary btn-block"
+                  :disabled="$v.registerForm.$invalid"
+                >
                   Criar conta
                 </button>
               </div>
